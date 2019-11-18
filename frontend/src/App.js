f2634dacf870221e6aa82489a1a6ac1e82e096f5
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2019-11-18 13:06:50 -0500

    Created a Login Page and properly added a route to that page within the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
+// local imports
+import Login from './components/Login';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <h1>Hearth and Steel</h1>
+      <Route exact path="/" component={Login} />
     </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = () => {
+const Login = props => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axiosWithAuth()
+            .post('', {
+                username: username,
+                password: password
+            })
+            .then(res => {
+                localStorage.setItem('token', res.data.payload);
+                props.history.push('');
+            })
+            .catch(err => console.log(err));
+    };
+
     return (
-        <div></div>
+        <>
+        <h2>Welcome to your new favorite way to share your recipes!</h2>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+            />
+            <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                />
+                <button type="submit">Log In</button>
+        </form>
+        </>
     );
 };
 
