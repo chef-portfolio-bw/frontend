e7bbadb0567fe8b6a248cd73ffab97c486742f57
Andrew Ackerman <andrew.ackers@gmail.com>
2019-11-18 16:17:58 -0800

    Nav styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
-import { black } from "@material-ui/core/colors";
+
 
 
 const useStyles = makeStyles({
     typography: {
-        color: black[500]
-    }
+        marginRight: "45%"
+    },
 
+    buttonContainer: {
+        display: "flex",
+    },
+
+    mainNav: {
+        backgroundColor: "#CA7512",
+        fontFamily: "Arimo"
+    }
 })
 
 
 const Navbar = () => {
     const classes = useStyles();
     return (
-        <AppBar position="static">
+        <AppBar position="static" className={classes.mainNav}>
             <Toolbar>
                 <IconButton edge="start" color="inherit" aria-label="menu">
                 </IconButton>
                 <Typography variant="h6" className={classes.typography}>
                     Hearth and Stone
                 </Typography>
-                <Button color="inherit">Recipes</Button>
-                <Button color="inherit">Profile</Button>
-                <Button color="inherit">Login</Button>
-                <Button color="inherit">Logout</Button>
+                <div className={classes.buttonContainer}>
+                    <Button color="inherit">Recipes</Button>
+                    <Button color="inherit">Profile</Button>
+                    <Button color="inherit">Login</Button>
+                    <Button color="inherit">Logout</Button>
+                </div>
+
             </Toolbar>
         </AppBar>
     );
